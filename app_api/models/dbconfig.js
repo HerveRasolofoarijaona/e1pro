a46ec0358d0aad1968a66573a82f746ff9bcda19
Herve <herve.rasolofoarijaona@esti.mg>
2019-08-15 18:47:05 +0300

    fixed untracked files

--- app_api/models/dbconfig.js
+++ app_api/models/dbconfig.js
@@ -1,7 +1,3 @@
-/**
- * Created by nokamojd on 15/07/2016.
- */
-
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //mongoose.connect("mongodb://epp:testepppdb@ds139645.mlab.com:39645/eappdb");
